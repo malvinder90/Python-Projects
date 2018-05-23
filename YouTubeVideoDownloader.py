fc3fa2c66a615eb8e007b57536ec252ebcfa2a68
malvinder90 <39308066+malvinder90@users.noreply.github.com>
2018-05-23 10:07:58 +0530

    Add files via upload

---
+++ YouTubeVideoDownloader.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+from tkinter import *
+
+def youtubedl():
+	try:
+		status.set("Download in progress")
+		yt = YouTube(url.get())
+		yt.streams.filter(file_extension="mp4").first().download()
+		status.set("Download complete for {}".format(yt.title))
+	except:
+		status.set("Download failed.")
+
+root = Tk()
+root.title("My YouTube Downloader")
+
+f = Frame()
+f.grid(column=0,row=3)
+
+url = StringVar()
+status = StringVar()
+
+Label(f,text="Enter YouTube video URL: ",width=60,fg="green").grid(column=0,row=0)
+Entry(f,textvariable=url,width=60,bg="yellow").grid(column=0,row=1)
+Label(f,textvariable=status,width=60).grid(column=0,row=2)
+Button(f,text="Download",command=youtubedl,width=20,bg="purple",fg="white").grid(column=0,row=3)
+root.mainloop()
